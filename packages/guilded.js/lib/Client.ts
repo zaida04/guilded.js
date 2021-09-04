d38e281b58d3d43ad760abeadeaa04b010f2d0e9
Skillz4Killz <23035000+Skillz4Killz@users.noreply.github.com>
2021-09-03 23:53:38 -0400

    feat: add Client class base code & @types/node dep (#9)
    * feat: add Client class base code & @types/node dep
    
    * fix: remove commit script in favor of husky commit hook
    
    * refactor: a little cleanup as advised
    
    * Update Client.ts
    
    * Update Client.ts
    
    Co-authored-by: Nico <me@zaid-arshad.tech>

---
+++ packages/guilded.js/lib/Client.ts
@@ -0,0 +1,48 @@
+import { EventEmitter } from "events";
+
+interface ClientOptions {
+    /** The bot's token */
+    token: string;
+}
+
+export class Client extends EventEmitter {
+    /** The time in milliseconds the Client connected */
+    readyTimestamp = 0;
+    
+    // TODO: Enable the manager once it is built.
+    /** The manager for the bot to make requests to the REST api. */
+    // rest: RestManager;
+
+    // TODO: Enable the manager once it is built
+    /** The gateway manager for the bot to manage all gateway connections through websockets. */
+    // gateway: GatewayManager;
+
+    constructor(public options: ClientOptions) {
+        super();
+        // TODO: Enable the manager once it is built
+        // this.gateway = new GatewayManager();
+        // this.rest = new RestManager();
+    }
+
+    /** The amount of time the bot has been online in milliseconds. */
+    get uptime() {
+        return this.readyTimestamp ? Date.now() - this.readyTimestamp : 0;
+    }
+
+    /** The bot's token. */
+    get token() {
+        return this.options.token;
+    }
+
+    /** Connects the bot to the api. */
+    async connect() {
+        return;
+    }
+
+    /** Disconnects the bot. */
+    async disconnect() {
+        return;
+    }
+}
+
+export default Client;
