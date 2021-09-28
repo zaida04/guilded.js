341ab8511c5fe5896ff131bdb09fbf807d6097e2
Skillz <gankstarsleague@gmail.com>
2021-09-28 02:35:41 -0400

    feat: add guilded status codes

---
+++ packages/rest/lib/constants.ts
@@ -0,0 +1,16 @@
+/** The meanings of each of the known guilded api response status codes. */
+export const GUILDED_STATUS_CODES = {
+    200: "The request was successful",
+    201: "The content was created",
+    204: "No content returned",
+    400: "The request was unacceptable, often due to missing parameters",
+    401: "The access token is missing or invalid",
+    403: "The bot does not have the necessary permissions",
+    404: "The requested resource does not exist",
+    409: "The request conflicted with another request",
+    500: "Something went wrong on our end",
+    501: "Something went wrong on our end",
+    502: "Something went wrong on our end",
+    503: "Something went wrong on our end",
+    504: "Something went wrong on our end",
+};
