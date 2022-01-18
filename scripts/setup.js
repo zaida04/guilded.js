675052758f7d3f2a286b554b24dc871241e959c4
Zaid (Nico) <me@zaid-arshad.tech>
2022-01-18 16:50:59 -0500

    chore: migrate to yarn

--- scripts/setup.js
+++ scripts/setup.js
@@ -1,5 +1,5 @@
 const { execSync } = require("child_process");
 const { join } = require("path");
 
-const commands = ["npm i", "npm run bootstrap", "npm run build"];
+const commands = ["yarn install", "yarn build"];
 commands.forEach((command) => execSync(command, { cwd: join(__dirname, "..") }));
