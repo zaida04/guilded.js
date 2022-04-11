393706bc2fb4fc7a1ced72c6f9b0e7c1d54d1644
Skillz4Killz <23035000+Skillz4Killz@users.noreply.github.com>
2022-04-11 12:30:37 +0000

    fix: add allowedIn inhibitor

---
+++ packages/framework/lib/inhibitors/allowedIn.ts
@@ -0,0 +1,25 @@
+import type { Message } from "guilded.js";
+
+import type { Command } from "../structures/Command";
+import { Inhibitor } from "../structures/Inhibitor";
+
+export default class AllowedInInhibitor extends Inhibitor {
+    name = "allowedIn";
+
+    execute(message: Message, command: Command): boolean {
+        // The command should be allowed to run because it meets the requirements
+        // allowedIn defaults to ["server"]
+        if ((!command.allowedIn || command.allowedIn.includes("server")) && message.serverId) return false;
+
+        // If the command is allowed in dms
+        if (command.allowedIn?.includes("dm") && !message.serverId) return false;
+
+        // THE COMMANDS NEEDS TO BE INHIBITED.
+        console.log(`${command.name} Inhibited: ALLOWED IN`);
+        return true;
+    }
+
+    init(): void {
+        // shut up eslint
+    }
+}
