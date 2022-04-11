43f38b3948bd25abe3f058e4ef939c47d8c31504
Skillz4Killz <23035000+Skillz4Killz@users.noreply.github.com>
2022-04-11 04:04:19 +0000

    fix: export commands monitor as default

--- packages/framework/lib/monitors/commands.ts
+++ packages/framework/lib/monitors/commands.ts
@@ -2,7 +2,7 @@ import type { Message } from "guilded.js";
 
 import { Monitor } from "../structures/Monitor";
 
-export class CommandsMonitor extends Monitor {
+export default class CommandsMonitor extends Monitor {
     // Commands should not ignore dms.
     ignoreDM = false;
 
