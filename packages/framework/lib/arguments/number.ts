3ce25c932caf52f0fb66f975cd46a701edb0d068
Skillz4Killz <23035000+Skillz4Killz@users.noreply.github.com>
2022-04-11 12:06:23 +0000

    fix: add number argument

---
+++ packages/framework/lib/arguments/number.ts
@@ -0,0 +1,23 @@
+import { Argument } from "../structures/Argument";
+import type { CommandArgument } from "../structures/Command";
+
+export default class NumberArgument extends Argument {
+    name = "number";
+
+    execute(argument: CommandArgument, parameters: string[]): number | undefined {
+        const [number] = parameters;
+
+        const valid = Number(number);
+        if (!valid) return;
+
+        if (valid < (argument.minimum || 0)) return;
+        if (argument.maximum && valid > argument.maximum) return;
+        if (!argument.allowDecimals) return Math.floor(valid);
+
+        if (valid) return valid;
+    }
+
+    init(): void {
+        // shut up eslint
+    }
+}
