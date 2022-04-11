2632a87f7abebd9ad742904aa2e6e6e6d8e9febb
Skillz4Killz <23035000+Skillz4Killz@users.noreply.github.com>
2022-04-11 12:10:39 +0000

    fix: add boolean argument

---
+++ packages/framework/lib/arguments/boolean.ts
@@ -0,0 +1,18 @@
+import { Argument } from "../structures/Argument";
+import type { CommandArgument } from "../structures/Command";
+
+export default class BooleanArgument extends Argument {
+    name = "boolean";
+
+    execute(argument: CommandArgument, parameters: string[]): boolean | undefined {
+        const [boolean] = parameters;
+
+        if (["true", "false", "on", "off", "enable", "disable"].includes(boolean)) {
+            return ["true", "on", "enable"].includes(boolean);
+        }
+    }
+
+    init(): void {
+        // shut up eslint
+    }
+}
