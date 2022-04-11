eeb1f033566f84e32e210d53fc5ba1bb6edb6f99
Skillz4Killz <23035000+Skillz4Killz@users.noreply.github.com>
2022-04-11 12:16:08 +0000

    fix: add subcommand argument

---
+++ packages/framework/lib/arguments/subcommand.ts
@@ -0,0 +1,22 @@
+import type { Message } from "guilded.js";
+
+import { Argument } from "../structures/Argument";
+import type { Command, CommandArgument } from "../structures/Command";
+
+export default class CommandTypeArgument extends Argument {
+    name = "subcommand";
+
+    execute(argument: CommandArgument, parameters: string[], message: Message, command: Command): Command | undefined {
+        const subcommandName = parameters[0]?.toLowerCase();
+        if (!subcommandName) return;
+
+        const sub = command.subcommands?.find((sub) => sub.name === subcommandName || Boolean(sub.aliases?.includes(subcommandName)));
+        if (sub) return sub;
+
+        return typeof argument.defaultValue === "string" ? command.subcommands?.get(argument.defaultValue) : undefined;
+    }
+
+    init(): void {
+        // shut up eslint
+    }
+}
