295ae5286ea4c453c17e0515c24eb7362da255b2
Skillz4Killz <23035000+Skillz4Killz@users.noreply.github.com>
2022-04-14 04:34:20 +0000

    fix: exports for gil

--- packages/framework/lib/index.ts
+++ packages/framework/lib/index.ts
@@ -1,7 +1,21 @@
 import BotClient from "./BotClient";
 
+export * from "./arguments/...string";
+export * from "./arguments/boolean";
+export * from "./arguments/command";
+export * from "./arguments/duration";
+export * from "./arguments/number";
+export * from "./arguments/string";
+export * from "./arguments/subcommand";
 export * from "./BotClient";
+export * from "./commands/ping";
+export * from "./inhibitors/allowedIn";
+export * from "./inhibitors/cooldown";
+export * from "./monitors/commands";
+export * from "./monitors/messageCollector";
+export * from "./structures/Argument";
+export * from "./structures/Command";
+export * from "./structures/Inhibitor";
 export * from "./structures/Monitor";
 export * from "./utils/walk";
-
 export default BotClient;
