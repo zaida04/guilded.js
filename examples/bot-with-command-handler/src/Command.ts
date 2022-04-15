b05cca45a087aa9865427600e29ee8a51d5405da
zaida04 <nico.03727+github@gmail.com>
2022-04-15 16:30:24 -0400

    chore: remove examples

--- examples/bot-with-command-handler/src/Command.ts
+++
@@ -1,7 +0,0 @@
-import type { Message } from "../../../packages/guilded.js/types/index.js";
-
-export interface Command {
-    aliases: string[];
-    execute: (msg: Message, args: string[]) => any;
-    name: string;
-}
