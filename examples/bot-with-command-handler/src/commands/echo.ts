5a83a0b391a13b854bae00760a5a128fdb921c6d
Nico <nico.03727+github@gmail.com>
2022-04-15 16:35:56 -0400

    chore: remove examples (#66)

--- examples/bot-with-command-handler/src/commands/echo.ts
+++
@@ -1,7 +0,0 @@
-import type { Command } from "../Command.js";
-
-export default {
-    aliases: ["speak", "talk"],
-    execute: (msg, args) => msg.client.messages.send(msg.channelId, args.join(" ")),
-    name: "echo",
-} as Command;
--- examples/bot-with-command-handler/src/commands/ping.ts
+++
@@ -1,7 +0,0 @@
-import type { Command } from "../Command.js";
-
-export default {
-    aliases: ["ping"],
-    execute: (msg) => msg.client.messages.send(msg.channelId, "pong!"),
-    name: "ping",
-} as Command;
