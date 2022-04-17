ac0d000030b8613d0696d2d19bf279e49d85813d
DaStormer <dastormer@stormdevelopmentz.xyz>
2022-04-17 19:19:49 -0400

    feat: roleUpdated event

--- packages/guilded.js/lib/gateway/handler/TeamEventHandler.ts
+++ packages/guilded.js/lib/gateway/handler/TeamEventHandler.ts
@@ -1,8 +1,20 @@
 import type { WSTeamRolesUpdatedPayload } from "@guildedjs/guilded-api-typings";
+import { constants } from "../../constants";
 import { GatewayEventHandler } from "./GatewayEventHandler";
 
 export class TeamEventHandler extends GatewayEventHandler {
     teamRolesUpdated(data: WSTeamRolesUpdatedPayload): boolean {
-        return false;
+        const newMembers = [];
+        const oldMembers = [];
+        for (const m of data.d.memberRoleIds) {
+            const member = this.client.members.cache.get(`${data.d.serverId}:${m.userId}`);
+            if (!member) {
+                newMembers.push(m);
+                continue;
+            }
+            oldMembers.push(member._clone());
+            newMembers.push(member._update({ roleIds: m.roleIds }));
+        }
+        return this.client.emit(constants.clientEvents.TEAM_ROLES_UPATED, data.d.serverId, newMembers, oldMembers)
     }
 }
