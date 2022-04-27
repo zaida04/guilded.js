0c2707fa6935e03b01855098a92ca7c64395eed2
zaida04 <nico.03727+github@gmail.com>
2022-04-27 18:41:59 -0400

    feat: remove message methods from channel

--- packages/guilded.js/lib/structures/channels/Channel.ts
+++ packages/guilded.js/lib/structures/channels/Channel.ts
@@ -4,11 +4,6 @@ import { Base } from "../Base";
 import type { Message } from "../Message";
 
 export class Channel extends Base {
-    /** Send a message to this channel. */
-    send(content: RESTPostChannelMessagesBody | string): Promise<Message> {
-        return this.client.messages.send(this.id, content);
-    }
-
     /** Get a list of the latest 100 messages from a channel. */
     fetchMessages(options?: RESTGetChannelMessagesQuery): Promise<Collection<string, Message>> {
         return this.client.messages.fetchMany(this.id, options ?? {});
@@ -17,20 +12,5 @@ export class Channel extends Base {
     /** Get details for a specific chat message from a chat channel. */
     fetchMessage(messageId: string): Promise<Message> {
         return this.client.messages.fetch(this.id, messageId);
-    }
-
-    /** Update a channel message. */
-    updateMessage(messageId: string, content: RESTPostChannelMessagesBody | string): Promise<Message> {
-        return this.client.messages.update(this.id, messageId, content);
-    }
-
-    /** Delete a channel message. */
-    deleteMessage(messageId: string): Promise<void> {
-        return this.client.messages.delete(this.id, messageId);
-    }
-
-    /** Add a reaction emote */
-    addReaction(contentId: string, emoteId: number): Promise<void> {
-        return this.client.messages.addReaction(this.id, contentId, emoteId);
     }
 }
