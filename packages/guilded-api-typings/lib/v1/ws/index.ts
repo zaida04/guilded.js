0b6384aa0b383da55e31633f4a0d9d8063c04445
zaida04 <nico.03727+github@gmail.com>
2022-05-01 13:09:29 -0400

    feat: fix export

--- packages/guilded-api-typings/lib/v1/ws/index.ts
+++ packages/guilded-api-typings/lib/v1/ws/index.ts
@@ -1,1 +1,7 @@
+export * from "./Channel";
+export * from "./Doc";
 export * from "./Events";
+export * from "./List";
+export * from "./Member";
+export * from "./Message";
+export * from "./Webhook";
