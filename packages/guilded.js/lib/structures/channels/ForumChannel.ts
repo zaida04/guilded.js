9e8f257a0a3983097e910c75f249ac500965b222
DaStormer <dastormer@stormdevelopmentz.xyz>
2022-06-11 20:33:02 -0400

    update instances of forum thread to topic

--- packages/guilded.js/lib/structures/channels/ForumChannel.ts
+++ packages/guilded.js/lib/structures/channels/ForumChannel.ts
@@ -3,8 +3,8 @@ import type { ForumTopicPayload } from "@guildedjs/guilded-api-typings";
 import { Channel } from "./Channel";
 
 export class ForumChannel extends Channel {
-    /** The threads in this channel. */
-    readonly threads = new Collection<string, ForumTopicPayload>();
+    /** The topics in this channel. */
+    readonly topics = new Collection<string, ForumTopicPayload>();
 
     /** Create a topic in a forum */
     createTopic(title: string, content: string): Promise<ForumTopicPayload> {
