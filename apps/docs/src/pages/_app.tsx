4d4ed55c42f23ecd758a23d6ceac8ae2c088d5ad
zaida04 <nico.03727+github@gmail.com>
2023-01-02 23:06:58 -0500

    chore: block until font loaded

--- apps/docs/src/pages/_app.tsx
+++ apps/docs/src/pages/_app.tsx
@@ -2,7 +2,7 @@ import '../styles/globals.css'
 import localFont from '@next/font/local'
 import type { AppProps } from 'next/app'
 
-const Mono = localFont({ src: '../styles/fonts/webfonts/JetBrainsMono-Regular.woff2', variable: '--font-mono' })
+const Mono = localFont({ src: '../styles/fonts/webfonts/JetBrainsMono-Regular.woff2', variable: '--font-mono', display: "swap" })
 export default function App({ Component, pageProps }: AppProps) {
 	return <main className={`${Mono.variable} font-sans`}>
 		<Component {...pageProps} />
