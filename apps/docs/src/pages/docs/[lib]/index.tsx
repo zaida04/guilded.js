7ce16d541c379351c4561947440621b747df189f
zaida04 <nico.03727+github@gmail.com>
2023-01-03 00:09:42 -0500

    feat(site): list of entities

--- apps/docs/src/pages/docs/[lib]/index.tsx
+++ apps/docs/src/pages/docs/[lib]/index.tsx
@@ -1,6 +1,5 @@
 import type { GetStaticPaths, GetStaticProps, NextPage } from "next";
 import { LayoutWrapper } from "../../../components/LayoutWrapper";
-import { Navbar } from "../../../components/Navbar";
 import fetchDocs from "../../../lib/loader";
 import { getUnscopedPackageName } from "../../../lib/util";
 
@@ -25,18 +24,28 @@ export const getStaticPaths: GetStaticPaths = async () => {
 	}
 }
 
+const EntityList = ({ name, entity }: { entity: string[], name: string }) => {
+	if (!Object.keys(entity).length) return null;
+
+	return <div>
+		<h1 className="text-white text-5xl font-bold pb-6 underline underline-offset-8">{name.slice(0, 1).toUpperCase() + name.slice(1)}</h1>
+		<div className="grid grid-rows-[auto] grid-cols-3 gap-x-[1rem]">
+			{entity.map(entity =>
+				<p className="text-xl" key={entity}>{entity}</p>
+			)}
+		</div>
+	</div>
+}
+
 const DocsPackage: NextPage<Props> = (props) => {
 	return <LayoutWrapper>
-		{
-			Object.keys(props).map(type =>
-				<div key={type}>
-					<h1 className="text-3xl">{type}</h1>
-					{props[type as keyof Props].map(entity =>
-						<p className="text-md" key={entity}>{entity}</p>
-					)}
-				</div>
-			)
-		}
+		<div className="my-16 ml-20 text-white grid grid-cols-none gap-8">
+			{Object
+				.keys(props)
+				.map(type =>
+					<EntityList entity={props[type as keyof typeof props]} key={type} name={type} />
+				)}
+		</div>
 	</LayoutWrapper>
 }
 
