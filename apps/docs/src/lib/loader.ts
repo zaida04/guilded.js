79d25424205fa7de7b0fe84e15c1acd33090efc1
zaida04 <nico.03727+github@gmail.com>
2023-01-07 21:32:50 -0500

    hotfix: ts-ignore docs output path

--- apps/docs/src/lib/loader.ts
+++ apps/docs/src/lib/loader.ts
@@ -1,4 +1,6 @@
 import type { JSONOutput } from "typedoc";
+// eslint-disable-next-line @typescript-eslint/prefer-ts-expect-error
+// @ts-ignore Depending on if docs were not generated, this import may fail.
 import docs from "../../../../docs/output.json";
 
 // potentially use some other way than local fs
