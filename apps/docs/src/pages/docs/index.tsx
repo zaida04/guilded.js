53e4abb34afdcd2aa357d99fb6e34b9936c7465f
zaida04 <nico.03727+github@gmail.com>
2023-01-08 00:21:48 -0500

    feat: bugs banner

--- apps/docs/src/pages/docs/index.tsx
+++ apps/docs/src/pages/docs/index.tsx
@@ -12,6 +12,18 @@ export const getStaticProps: GetStaticProps<Props> = async () => {
 
 const Docs: NextPage<Props> = ({ packages }) => {
 	return <LayoutWrapper>
+		<div className="flex justify-center">
+			<div className="bg-orange-100 border-l-4 border-orange-500 text-orange-700 p-4 w-3/4" role="alert">
+				<p className="font-bold">Be Warned</p>
+				<p>
+					All the docs pages below "Advanced Docs" are currently in development.
+					There will likely be bugs.
+					They're meant to be a simplified version of the typedoc docs, but they will certainly be lacking for those who are advanced users.{" "}
+					<a href="https://github.com/guildedjs/guilded.js/issues"><b>Please report bugs here!</b></a>
+				</p>
+			</div>
+		</div>
+
 		<div className="min-h-screen flex items-center flex-col pt-12">
 			<h1 className="text-5xl text-white font-bold pb-4">Packages:</h1>
 			<a href="https://guilded.js.org/">
