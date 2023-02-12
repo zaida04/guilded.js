5b632f0c4d3910ab57726837c6e1b73848997364
zaida04 <nico.03727+github@gmail.com>
2023-02-11 21:58:50 -0500

    chore: add icon to packages

--- apps/docs/src/components/PackageBadge.tsx
+++ apps/docs/src/components/PackageBadge.tsx
@@ -1,9 +1,12 @@
+import { faBox } from "@fortawesome/free-solid-svg-icons";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 import Link from "next/link";
 import { getUnscopedPackageName } from "../lib/util";
 
 export const PackageBadge = ({ text }: { text: string }) =>
 	<Link href={`/docs/${getUnscopedPackageName(text)}`}>
-		<div className="p-4 mt-2 bg-black rounded-lg max-w-fit transition-transform transform hover:scale-110">
+		<div className="flex p-4 mt-2 bg-black rounded-lg max-w-fit transition-transform transform hover:scale-110">
+			<FontAwesomeIcon className="text-2xl text-zinc-700 pr-4" icon={faBox} />
 			<p className="text-md md:text-xl text-white font-bold">{text}</p>
 		</div>
 	</Link>
