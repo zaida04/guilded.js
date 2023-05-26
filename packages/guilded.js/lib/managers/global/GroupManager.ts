0b48edaee2a4a1c6c699abc9c87adc2cc991b789
zaida04 <nico.03727+github@gmail.com>
2023-05-26 17:12:38 -0400

    feat: group CRUD

--- packages/guilded.js/lib/managers/global/GroupManager.ts
+++ packages/guilded.js/lib/managers/global/GroupManager.ts
@@ -1,9 +1,106 @@
+import { GroupsService } from "@guildedjs/api";
+import { OptionBody } from "../../typings";
 import { GlobalManager } from "./GlobalManager";
+import { Group } from "../../structures";
+import { CacheableStructManager } from "./CacheableStructManager";
+import { Collection } from "@discordjs/collection";
 
 /**
  * Manager for interacting with Groups on Guilded.
  */
-export class GlobalGroupManager extends GlobalManager {
+export class GlobalGroupManager extends CacheableStructManager<string, Group> {
+  /**
+   * Creates a group.
+   * @param serverId The ID of the server to create the group in.
+   * @param options The options for the group to create.
+   * @returns A Promise that resolves with the created group.
+   */
+  create(
+    serverId: string,
+    options: OptionBody<GroupsService["groupCreate"]>
+  ): Promise<Group> {
+    return this.client.rest.router.groups
+      .groupCreate({ serverId, requestBody: options })
+      .then((data) => {
+        const createdGroup = new Group(this.client, data.group);
+        this.cache.set(createdGroup.id, createdGroup);
+        return createdGroup;
+      });
+  }
+
+  /**
+   * Fetch a group
+   * @param serverId The ID of the server to fetch the group from.
+   * @param groupId The ID of the group to fetch.
+   * @returns A Promise that resolves with the fetched group.
+   */
+  fetch(serverId: string, groupId: string): Promise<Group> {
+    return this.client.rest.router.groups
+      .groupRead({ serverId, groupId })
+      .then((data) => {
+        const fetchedGroup = new Group(this.client, data.group);
+        this.cache.set(fetchedGroup.id, fetchedGroup);
+        return fetchedGroup;
+      });
+  }
+
+  /**
+   * Fetch all the groups in a server
+   * @param serverId The ID of the server to fetch the groups from.
+   * @returns A Promise that resolves a Collection containing the fetched groups.
+   */
+  fetchMany(serverId: string): Promise<Collection<string, Group>> {
+    return this.client.rest.router.groups
+      .groupReadMany({ serverId })
+      .then((data) => {
+        const groups = new Collection<string, Group>();
+
+        for (const group of data.groups) {
+          const fetchedGroup = new Group(this.client, group);
+          this.cache.set(fetchedGroup.id, fetchedGroup);
+          groups.set(fetchedGroup.id, fetchedGroup);
+        }
+
+        return groups;
+      });
+  }
+
+  /**
+   * Update a group
+   * @param serverId The ID of the server to update the group in.
+   * @param groupId The ID of the group to update.
+   * @param options The options to update the group with.
+   * @returns A Promise that resolves with the updated group.
+   */
+  update(
+    serverId: string,
+    groupId: string,
+    options: OptionBody<GroupsService["groupUpdate"]>
+  ): Promise<Group> {
+    return this.client.rest.router.groups
+      .groupUpdate({ serverId, groupId, requestBody: options })
+      .then((data) => {
+        let group = this.cache.get(data.group.id)?._update(data.group);
+        group ??= new Group(this.client, data.group);
+
+        return group;
+      });
+  }
+
+  /**
+   * Delete a group
+   * @param serverId The ID of the server to delete the group from.
+   * @param groupId The ID of the group to delete.
+   * @returns A Promise that resolves when the operation is complete.
+   */
+  delete(serverId: string, groupId: string): Promise<void> {
+    return this.client.rest.router.groups
+      .groupDelete({ serverId, groupId })
+      .then(() => {
+        if (this.cache.has(groupId)) this.cache.delete(groupId);
+      });
+  }
+
   /**
    * Adds a member to a group.
    * @param groupId The ID of the group.
--- packages/guilded.js/lib/structures/Group.ts
+++ packages/guilded.js/lib/structures/Group.ts
@@ -1,9 +1,80 @@
+import { GroupPayload } from "@guildedjs/api";
 import { Base } from "./Base";
+import { Client } from "./Client";
 
 /**
  * A group in a server in Guilded
  */
 export class Group extends Base {
+  /** The name of the group */
+  public name!: string;
+
+  /** The description of the group */
+  public description!: string;
+
+  /** The ID of the server this group is in */
+  public serverId: string;
+
+  /** The ID of the user who created this group */
+  public createdBy: string;
+
+  /** The date this group was created at */
+  public createdAt: Date;
+
+  /** The date this group was last updated at */
+  public updatedAt!: Date;
+
+  // Avatar of this group
+  public avatar!: string;
+
+  // Whether this is the main group in a server
+  public isHome!: boolean;
+
+  // ID of the emote for this group
+  public emoteId!: number;
+
+  // Whether this group is public
+  public isPublic!: boolean;
+
+  // ID of the user who last updated this group
+  public updatedBy!: string;
+
+  // Date this group was archived if it was archived
+  public archivedAt!: Date | null;
+
+  // ID of the user who archived this group if it was archived
+  public archivedBy!: string | null;
+
+  constructor(client: Client, data: GroupPayload) {
+    super(client, data);
+    this.serverId = data.serverId;
+    this.createdAt = new Date(Date.parse(data.createdAt));
+    this.createdBy = data.createdBy;
+
+    this._update(data);
+  }
+
+  _update(data: GroupPayload): this {
+    if (typeof data.name !== "undefined") this.name = data.name;
+    if (typeof data.description !== "undefined")
+      this.description = data.description;
+    if (typeof data.updatedAt !== "undefined")
+      this.updatedAt = new Date(Date.parse(data.updatedAt));
+    if (typeof data.avatar !== "undefined") this.avatar = data.avatar;
+    if (typeof data.isHome !== "undefined") this.isHome = data.isHome;
+    if (typeof data.emoteId !== "undefined") this.emoteId = data.emoteId;
+    if (typeof data.isPublic !== "undefined") this.isPublic = data.isPublic;
+    if (typeof data.updatedBy !== "undefined") this.updatedBy = data.updatedBy;
+    if (typeof data.archivedAt !== "undefined")
+      this.archivedAt = data.archivedAt
+        ? new Date(Date.parse(data.archivedAt))
+        : null;
+    if (typeof data.archivedBy !== "undefined")
+      this.archivedBy = data.archivedBy;
+
+    return this;
+  }
+
   /**
    * Adds a member to the group.
    * @param memberId The ID of the member to add.
