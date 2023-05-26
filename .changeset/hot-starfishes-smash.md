ea41fe27ee1f20c8d583273702f3cbf9438b2bb6
zaida04 <nico.03727+github@gmail.com>
2023-05-26 16:36:21 -0400

    feat: client.users.fetchServers

---
+++ .changeset/hot-starfishes-smash.md
@@ -0,0 +1,5 @@
+---
+"guilded.js": patch
+---
+
+feat: client.users.fetchServers
--- packages/guilded.js/lib/managers/global/UserManager.ts
+++ packages/guilded.js/lib/managers/global/UserManager.ts
@@ -1,5 +1,7 @@
+import { Collection } from "@discordjs/collection";
 import { User } from "../../structures/User";
 import { CacheableStructManager } from "./CacheableStructManager";
+import { Server } from "../../structures/Server";
 
 /**
  * A manager for interacting with users. You can retrieve users from the .cache property.
@@ -21,5 +23,29 @@ export class GlobalUserManager extends CacheableStructManager<string, User> {
     return this.client.rest.router.users
       .userRead({ userId: "@me" })
       .then((data) => new User(this.client, data.user));
+  }
+
+  /**
+   * Fetches a user's servers (atm, only the logged in client is supported)
+   * @param userId The ID of the user to fetch servers for.
+   * @returns A promise that resolves to a Collection with the returned servers.
+   */
+  fetchServers(userId: string): Promise<Collection<string, Server>> {
+    if (userId !== this.client.user!.id)
+      throw new Error("Only the logged in client is supported at this time.");
+
+    return this.client.rest.router.users
+      .userServerReadMany({ userId })
+      .then((data) => {
+        const servers = new Collection<string, Server>();
+
+        for (const server of data.servers) {
+          const createdServer = new Server(this.client, server);
+          servers.set(server.id, createdServer);
+          this.client.servers.cache.set(server.id, createdServer);
+        }
+
+        return servers;
+      });
   }
 }
