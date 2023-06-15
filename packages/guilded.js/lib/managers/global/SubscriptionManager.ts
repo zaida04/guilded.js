380dc0142b3ff98e41179b7586340dc4fe35f5b5
Dylan Warren <dylanwarren19@gmail.com>
2023-06-15 16:12:53 -0500

    feat: server subscription tier manager and class

---
+++ packages/guilded.js/lib/managers/global/SubscriptionManager.ts
@@ -0,0 +1,50 @@
+import {
+  ServerSubscriptionTier,
+  ServerSubscriptionTierType,
+} from "../../structures/Subscription";
+import { GlobalManager } from "./GlobalManager";
+
+/**
+ * A class representing a global subscription tier manager.
+ */
+export class GlobalSubscriptionManager extends GlobalManager {
+  /**
+   * Fetches a subscription tier from a server.
+   * @param serverId The ID of the server to fetch the subscription tier from.
+   * @param serverSubscriptionTierType The subscription tier to fetch.
+   * @returns A Promise that resolves with the fetched subscription tier.
+   */
+  async fetch(
+    serverId: string,
+    serverSubscriptionTierType: ServerSubscriptionTierType
+  ): Promise<ServerSubscriptionTier> {
+    const data =
+      await this.client.rest.router.serverSubscriptions.serverSubscriptionTierRead(
+        { serverId, serverSubscriptionTierType }
+      );
+    return new ServerSubscriptionTier(this.client, {
+      ...data.serverSubscriptionTier,
+    });
+  }
+
+  /**
+   * Fetches all the subscription tiers from a server.
+   * @param serverId The ID of the server to fetch subscription tiers from.
+   * @returns A Promise that resolves with an array of subscription tiers.
+   */
+  async fetchMany(serverId: string): Promise<ServerSubscriptionTier[]> {
+    const data =
+      await this.client.rest.router.serverSubscriptions.serverSubscriptionTierReadMany(
+        {
+          serverId,
+        }
+      );
+    const serverSubscriptionTiers = [];
+    for (const tier of data.serverSubscriptionTiers) {
+      serverSubscriptionTiers.push(
+        new ServerSubscriptionTier(this.client, tier)
+      );
+    }
+    return serverSubscriptionTiers;
+  }
+}
---
+++ packages/guilded.js/lib/structures/Subscription.ts
@@ -0,0 +1,52 @@
+import type { ServerSubscriptionTierPayload } from "@guildedjs/api";
+import { Base } from "./Base";
+import { Client } from "./Client";
+
+/**
+ * Describes a subscription tier type.
+ */
+export type ServerSubscriptionTierType = "Gold" | "Silver" | "Copper";
+
+/**
+ * Represents a Guilded SubscriptionTier in a server.
+ */
+export class ServerSubscriptionTier extends Base<
+  ServerSubscriptionTierPayload,
+  ServerSubscriptionTierType
+> {
+  /** The ID of the server */
+  readonly serverId: string;
+
+  /** The ISO 8601 timestamp that the server subscription tier was created at */
+  _createdAt: number;
+
+  /** The type of the server subscription tier. This field is case sensitive!! */
+  type: ServerSubscriptionTierType;
+
+  /** The description associated with the server subscription tier (max length 256) */
+  description?: string;
+
+  /** The ID of the role */
+  roleId?: number;
+
+  /** The cost of the tier in cents USD per month (min 200; max 10000) */
+  cost: number;
+
+  /**
+   * @param client The client instance
+   * @param data The data for this role
+   */
+  constructor(client: Client, data: ServerSubscriptionTierPayload) {
+    super(client, { id: data.type, ...data });
+    this.serverId = data.serverId;
+    this._createdAt = Date.parse(data.createdAt);
+    this.description = data.description;
+    this.roleId = data.roleId;
+    this.cost = data.cost;
+    this.type = data.type;
+  }
+
+  get createdAt(): Date {
+    return new Date(this._createdAt);
+  }
+}
